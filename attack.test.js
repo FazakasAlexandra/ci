37b188fabfd25957c0972f4eb6809afc38643ecb
FazakasAlexandra <alexandra.fazakas91@gmail.com>
2022-09-23 11:14:57 +0300

    add tests

---
+++ attack.test.js
@@ -0,0 +1,7 @@
+const attack = require('./attack')
+
+test('should return dmg sentence when called correctly', () => {
+    const result = attack('Smelly ogre', 23)
+    //expect(result).toBe('Smelly ogre dealt 23 dmg')
+    expect(result).toMatch(/^Smelly ogre(.*)23/)
+})
